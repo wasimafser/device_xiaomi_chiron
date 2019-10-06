edd62f718e76066ea9e37369a5f60ec05c59bcbe
wasimafser <wasim.afser@gmail.com>
2019-10-06 17:21:39 +0530

    PA-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_chiron.mk
+    $(LOCAL_DIR)/pa_chiron.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_chiron-user \
-    lineage_chiron-userdebug \
-    lineage_chiron-eng
+    pa_chiron-user \
+    pa_chiron-userdebug \
+    pa_chiron-eng
